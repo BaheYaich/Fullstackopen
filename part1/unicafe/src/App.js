6ae53465681d9c35322f2aca288251609cdb6ecf
Bahe Yaich <Bahe.yaich@live.ca>
2023-02-10 23:22:06 +0200

    Exercise 1.6: Added matching initial values

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,9 +2,9 @@ import { useState } from 'react'
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(6)
+  const [neutral, setNeutral] = useState(2)
+  const [bad, setBad] = useState(1)
 
   return (
     <div>
