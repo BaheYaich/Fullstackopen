8e4f49e6a61e028344b3e768d1ff8cef8e18a84f
Bahe Yaich <Bahe.yaich@live.ca>
2023-02-10 23:55:22 +0200

    Exercise 1.6: Unicafé Step-1 (Added functions)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,16 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(6)
-  const [neutral, setNeutral] = useState(2)
-  const [bad, setBad] = useState(1)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {setGood(good + 1)}
+  const handleNeutral = () => {setNeutral(neutral + 1)}
+  const handleBad = () => {setBad(bad + 1)}
 
   return (
     <div>
       <h2>Give feedback</h2>
-      <button onClick={setGood}>Good</button>
-      <button onClick={setNeutral}>Neutral</button>
-      <button onClick={setBad}>Bad</button>
+      <button onClick={handleGood}>Good</button>
+      <button onClick={handleNeutral}>Neutral</button>
+      <button onClick={handleBad}>Bad</button>
       <h2>Statistics</h2>
       <p>Good : { good }</p>
       <p>Neutral : { neutral }</p>
