28e0d479eb47f7defd9a002df3d245b18100d1de
Bahe Yaich <Bahe.yaich@live.ca>
2023-02-11 19:58:34 +0200

    Exercise 1.6: Unicafe Step-2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,6 +5,10 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positivePercentage = (good / all) * 100
 
   const handleGood = () => {setGood(good + 1)}
   const handleNeutral = () => {setNeutral(neutral + 1)}
@@ -20,6 +24,9 @@ const App = () => {
       <p>Good : { good }</p>
       <p>Neutral : { neutral }</p>
       <p>Bad : { bad }</p>
+      <p>All: { all }</p>
+      <p>Average : { average }</p>
+      <p>Positive : { positivePercentage } %</p>
     </div>
   )
 }
