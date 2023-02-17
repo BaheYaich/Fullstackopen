97f8932284496f4248d6cbbd4422cee4ce3e7b30
Bahe Yaich <Bahe.yaich@live.ca>
2023-02-17 23:07:06 +0200

    Exercice 2.9: Phone book Step-4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,10 +4,15 @@ const Person = ({persons}) => <p>{persons.name} {persons.number}</p>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '+358410000000' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+  const filteredContacts = search === '' ? persons : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
   const handleNewPerson = (event) => {
     event.preventDefault()
     if (persons.find(person => person.name === newName)) return window.alert(`${newName} already exists in the phonebook`);
@@ -25,6 +30,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>Filter shown with: <input name="searchbar" onChange={e => setSearch(e.target.value)} value={search}/></div>
+      <h2>Add new contact</h2>
       <form onSubmit={handleNewPerson}>
         <div>
           name: <input name="name" onChange={e => setNewName(e.target.value)} value={newName} />
@@ -35,7 +42,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(persons => <Person key={persons.name} persons={persons} />)}
+      {filteredContacts.map(persons => <Person key={persons.name} persons={persons} />)}
     </div>
   )
 }
