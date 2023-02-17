3b2077f9327ebc397c3e0576b947548e68c44043
Bahe Yaich <Bahe.yaich@live.ca>
2023-02-17 17:38:10 +0200

    Exercice 2.8: Phone book Step-3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,31 +1,35 @@
 import { useState } from 'react'
 
-const Person = ({persons}) => <p>{persons.name}</p>
+const Person = ({persons}) => <p>{persons.name} {persons.number}</p>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', }
+    { name: 'Arto Hellas', number: '+358410000000' }
   ]) 
   const [newName, setNewName] = useState('')
-  const handleNewName = (event) => {
+  const [newNumber, setNewNumber] = useState('')
+  const handleNewPerson = (event) => {
     event.preventDefault()
     if (persons.find(person => person.name === newName)) return window.alert(`${newName} already exists in the phonebook`);
     else {
-    const person = {
-      name: newName,
-    }
+      const person = {
+        name: newName,
+        number: newNumber,
+      }
     setPersons(persons.concat(person))
     setNewName("")
+    setNewNumber("")
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleNewName}>
+      <form onSubmit={handleNewPerson}>
         <div>
           name: <input name="name" onChange={e => setNewName(e.target.value)} value={newName} />
         </div>
+        <div>number: <input name="number" onChange={e => setNewNumber(e.target.value)} value={newNumber}/></div>
         <div>
           <button type="submit">add</button>
         </div>
