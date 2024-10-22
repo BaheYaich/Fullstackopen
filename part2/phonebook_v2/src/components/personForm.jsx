16d362ec651f9b196efc3fcfc4d3d6054726f76c
Bahe <bahe.yaich@live.ca>
2024-10-22 20:13:47 +0300

    Exercise 3: Final touches

--- part2/phonebook_v2/src/components/personForm.jsx
+++ part2/phonebook_v2/src/components/personForm.jsx
@@ -1,8 +1,8 @@
 const PersonForm = ({functionReference, setNewName, newName, setNewNumber, newNumber}) => {
   return (
     <form onSubmit={functionReference}>
-        <label>name:</label><input name="name" onChange={e => setNewName(e.target.value)} value={newName} />
-        <label>number:</label><input name="number" onChange={e => setNewNumber(e.target.value)} value={newNumber}/>
+        <label>name:</label><input required placeholder="Type a name." name="name" onChange={e => setNewName(e.target.value)} value={newName} />
+        <label>number:</label><input required placeholder="Format XX-XXXXXXX or XXX-XXXXXXXX." name="number" onChange={e => setNewNumber(e.target.value)} value={newNumber}/>
         <button type="submit">add</button>
     </form>
   )
--- part2/phonebook_v2/src/components/search.jsx
+++ part2/phonebook_v2/src/components/search.jsx
@@ -1,3 +1,3 @@
-const Filter = ({setSearch, search}) => <div className="filterInput"><label>Filter shown with:</label> <input name="searchbar" onChange={e => setSearch(e.target.value)} value={search}/></div>
+const Filter = ({setSearch, search}) => <div className="filterInput"><label>Filter shown with:</label> <input placeholder="Type to start filtering" name="searchbar" onChange={e => setSearch(e.target.value)} value={search}/></div>
 
 export default Filter
